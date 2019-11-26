350f9c8829f1f2dbb9fb6c95af031bfc7d045385
Carlos <csan85@outlook.com>
2019-11-26 00:37:00 +0000

    Home

---
+++ mini.js
@@ -0,0 +1,26 @@
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+
+
+
+hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("open");
+    links.forEach(link => {
+        link.classList.toggle('fade');
+    });
+});
+
+function initMap() {
+    var location = {lat:51.465952 , lng:-0.072594}
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 15,
+        center: location
+    });
+    var marker = new google.maps.Marker({
+        position: location,
+        map: map
+    });
+}
+
+
